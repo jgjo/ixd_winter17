a2e0b1d7acf813995d43be499cc2b64dbf2a0621
Janet Johnson <janet.johnson8609@gmail.com>
2017-02-09 22:53:16 -0800

    add routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'IXD' });
 });
 
+/* GET logexperience page. */
+router.get('/logexperience', function(req, res, next) {
+  res.render('logexperience', { title: 'IXD' });
+});
+
+/* GET chat page. */
+router.get('/chat', function(req, res, next) {
+  res.render('chat', { title: 'IXD' });
+});
+
+/* GET viememories page. */
+router.get('/viewmemories', function(req, res, next) {
+  res.render('viewmemories', { title: 'IXD' });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'IXD' });
+});
+
 module.exports = router;
